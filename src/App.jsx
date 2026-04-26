6c363e6cb264e46c80a25a7bf4930c619ffb299b
TaskDaggerTheNerd <joao.luis.cunha.silva@gmail.com>
2026-04-26 18:44:35 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -582,8 +582,8 @@ const myOpenSplitTotal = useMemo(
 
 // Net: positive = partner owes you, negative = you owe partner
 const netSplitBalance = useMemo(
-  () => myOpenSplitTotal,
-  [myOpenSplitTotal]
+  () => myOpenSplitTotal - partnerOpenSplitTotal,
+  [myOpenSplitTotal, partnerOpenSplitTotal]
 )
 
 const splitBalanceLabel = useMemo(() => {
